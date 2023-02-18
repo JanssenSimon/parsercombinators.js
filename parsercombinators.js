6625e2660ba7bcc9618ea704f2cbce5c065e706e
Simon Janssen <32472583+JanssenSimon@users.noreply.github.com>
2023-02-18 14:14:39 -0500

    Improves how errors are handled and seperates examples of use into it's own section

--- parsercombinators.js
+++ parsercombinators.js
@@ -6,31 +6,36 @@ const addParensInRegexForSplit =
   re => new RegExp("("+(""+re).slice(1,(""+re).lastIndexOf("/"))+")")
 
 const isString = myVar => typeof myVar === 'string' || myVar instanceof String
-const isError = isString
+const isError = v => Array.isArray(v) && v.length === 2 && Number.isFinite(v[0])
 
-// recognizer
+// BASIC PARSER
 // returns a parser which recognizes specified pattern at start of string
-match = pattern => (
+// A parser takes a string and returns a tuple of an AST which the parser
+// accepts and the rest of the input
+// a parser may also return a string in the case of an error
+match = (pattern, errorMsg = null) => (
   input => {
     re = new RegExp(pattern) //cast to regex
     const [test,...[head,...tail]] = input.split(addParensInRegexForSplit(re))
-    return test == '' ? [head,tail.join('')] : `Error: Could not match ${re}`
+    let err_msg = isString(errorMsg) ? errorMsg : `Error: Could not match ${re}`
+    return test == '' ? [head,tail.join('')] : [0, err_msg]
   }
 )
 
 
 // BASIC COMBINATORS
-
 // sequence parser combinator ⊛ 
 andThen = (parser1, parser2) => (
   input => {
     let res1 = parser1(input)
-    if(isError(res1)) return res1   //TODO output where in code the error is
+    if (isError(res1)) return res1
     const [firstParsed,restInput1] = res1
     let res2 = parser2(restInput1)
-    if(isError(res2)) return res2
+    if (isError(res2))
+      return [(input.length - restInput1.length) + res2[0],res2[1]]
     const [secondParsed,restInput2] = res2
     return [[firstParsed,secondParsed],restInput2]
+    //TODO maybe? return [[firstParsed,secondParsed].flat(Infinity),restInput2]
   }
 )
 
@@ -41,41 +46,49 @@ orElse = (parser1, parser2, custom_error = null) => (
     if (!isError(res1)) return res1
     let res2 = parser2(input)
     if (!isError(res2)) return res2
-    return isError(custom_error) ? custom_error : res2
+    return isString(custom_error) ? [res1[0],custom_error] : res1
   }
 )
 
 // map parser combinator
-// TODO use pipe() and helper functions to make this and the functions above pretty
 map = (parser, func) => (
   input => {
     let res1 = parser(input)
-    if(isError(res1)) return res1
+    if (isError(res1)) return res1
     const [parsed,restInput] = res1
     return [func(parsed),restInput]
   }
 )
 
 
-// COMPLEX COMBINATORS
+// Errors
 
+// TODO actually display the line instead of 10 chars before and after
+// TODO alos display on a newline the position of the using '^'
+const displayline = (wholestr, pos) =>
+  wholestr.substring(Math.max(0, pos-10), Math.min(pos+10, wholestr.length))
+
+displayError = (parser) => (
+  input => {
+    const res = parser(input)
+    if (isError(res)) {
+      //const error_position = res[0]
+      //const error_message = res[1]
+      console.log(displayline(input, res[0]) + `\n ${res[1]}`)
+    }
+    return res
+  }
+)
+
+
+// COMPLEX COMBINATORS
 choice = parsers => parsers.reduce(orElse)
 
-parseLowercase = match(/[a-z]/)
-parseUppercase = match(/[A-Z]/)
-parseChar = orElse(parseLowercase, parseUppercase, 'Error: Could not match character')
-parseDigit = match(/[0-9]/)
-parseWhitespace = match(/\s+/)
 
-keepRight = (parser1, parser2) => map(andThen(parser1, parser2),
-                                      x => {const [l,r] = x; return r})
-keepLeft = (parser1, parser2) => map(andThen(parser1, parser2),
-                                     x => {const [l,r] = x; return l})
-
-function parseWordHelper(x) {
-    return orElse(keepLeft(parseChar, parseWhitespace),
-                  andThen(parseChar, parseWordHelper),
-                  'Error: Could not match word')(x)
-}
-parseWord = map(parseWordHelper,
-                x => [x].flat(Infinity).join(''))
+// Useful basic examples
+parseLowercase = match(/[a-z]/, 'Error: could not match lowercase char')
+parseUppercase = match(/[A-Z]/, 'Error: could not match uppercase char')
+parseChar = orElse(parseLowercase, parseUppercase, 'Error: Could not match char')
+parseDigit = match(/[0-9]/, 'Error: could not match digit')
+parseWhitespace = match(/\s+/, 'Error: could not match whitespace')
+parseWord = match(/[A-Za-z]+/, 'Error: Could not match word')
